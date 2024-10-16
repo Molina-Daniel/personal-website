a9fd00295e82805b8c608fe591e341e263c0e42c
Daniel Molina <danielmolina.dev@gmail.com>
2024-10-16 17:51:35 +0200

    feat: add nav header

---
+++ lib/data.ts
@@ -0,0 +1,22 @@
+export const sections = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
